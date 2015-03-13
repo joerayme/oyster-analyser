6ff286978836308e0ba4f5be3afa7945410e6344
Joe Ray <jray@thoughtworks.com>
2015-03-13 07:52:49 +0000

    Add test-refresh task

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
                  [clj-time "0.9.0"]]
   :main ^:skip-aot oyster-analyser.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.6.0"]]}})
