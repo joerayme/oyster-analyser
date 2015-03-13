36a7621ac0b6da7108a14f7f7d1f13cbbf11b746
Joe Ray <jray@thoughtworks.com>
2015-03-13 07:35:58 +0000

    Print summary table when -main is run

--- src/oyster_analyser/core.clj
+++ src/oyster_analyser/core.clj
@@ -1,7 +1,30 @@
 (ns oyster-analyser.core
+  (:require [oyster-analyser.data :refer :all]
+            [oyster-analyser.analyse :refer :all]
+            [clojure.pprint :refer [pprint get-pretty-writer cl-format]]
+            [clojure.string :as s])
   (:gen-class))
 
+(def key-mapping {:totalDuration "Total Duration"
+                  :averageDuration "Avg. Duration"
+                  :totalCost "Total Cost"
+                  :averageCost "Avg. Cost"})
+
+(defn- print-table [aseq column-width]
+  (binding [*out* (get-pretty-writer *out*)]
+    (doseq [row aseq]
+      (cl-format true " ~A~v,1T ~7,2F"
+                 ((first row) key-mapping)
+                 (+ column-width 3)
+                 (second row)
+                 )
+      (prn))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [summary (summarise (flatten (map #(convert (slurp %)) args)))
+        max-title (apply max (map count (map #(% key-mapping) (keys summary))))]
+    (println)
+    (print-table summary max-title)
+    (println)
+    ))
