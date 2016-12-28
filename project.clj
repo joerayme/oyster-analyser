762105307c5c69d76ebe7ccd94dbf02b7472c109
Joe Ray <jray@thoughtworks.com>
2016-12-28 11:38:23 +0000

    Upgrade Clojure dependency and lein-test-refresh plugin

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [clj-time "0.9.0"]]
   :main ^:skip-aot oyster-analyser.core
   :target-path "target/%s"
   :uberjar-name "oyster-analyser-uber.jar"
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.6.0"]]}})
+             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]]}})
