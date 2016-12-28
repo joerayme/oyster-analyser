76e1e6018872a8b5bb5ff8dec0fcd71821539dfa
Joe Ray <jray@thoughtworks.com>
2016-12-28 11:38:59 +0000

    Ensure make-table test passes regardless of vector order

--- test/oyster_analyser/core_test.clj
+++ test/oyster_analyser/core_test.clj
@@ -12,8 +12,8 @@
                              :totalCost (BigDecimal. "137.78")
                              :averageCost (BigDecimal. "1.78")
                              :totalJourneys 77
-                             :mostPopularType '("bus" 44)})]
-      (is (= table [["Total Duration" "13 hrs 8 mins"]
+                             :mostPopularType '("bus" 44)})
+          expected [["Total Duration" "13 hrs 8 mins"]
                     ["Avg. Duration" "24 mins"]
                     ["Shortest Journey" "9 mins"]
                     ["Longest Journey" "58 mins"]
@@ -21,7 +21,8 @@
                     ["Total Cost" "£137.78"]
                     ["Avg. Cost" "£1.78"]
                     ["Journeys" "77"]
-                    ["Most popular mode" "Bus (57%)"]]))
+                    ["Most popular mode" "Bus"]]]
+      (is (= (sort-by first expected) (sort-by first table)))
       )
     )
   )
